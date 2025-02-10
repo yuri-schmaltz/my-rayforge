cc89503fe75e40df0cf96898e4a7abe4f4c8d7d4
Yuri Schmaltz <security@yuri-schmaltz.dev>
2025-02-10 18:10:05 +0100

    add Optimizer modifier, to aid with debugging the path optimizer

--- rayforge/modifier/__init__.py
+++ rayforge/modifier/__init__.py
@@ -4,6 +4,7 @@ from .modifier import Modifier
 from .transparency import MakeTransparent
 from .grayscale import ToGrayscale
 from .outline import OutlineTracer
+from .optimize import Optimizer
 from .rasterize import Rasterizer
 
 modifier_by_name = dict(
