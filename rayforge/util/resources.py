e41f989096fe512cb221c7e187837636a883024e
Yuri Schmaltz <security@yuri-schmaltz.dev>
2025-02-16 16:18:49 +0100

    remove unused resource file

--- rayforge/util/resources.py
+++ rayforge/util/resources.py
@@ -1,6 +1,6 @@
 import importlib.resources
 from gi.repository import Gtk
-from ..resources import icons, machines
+from ..resources import icons
 
 
 def get_icon_path(icon_name):
@@ -12,9 +12,3 @@ def get_icon_path(icon_name):
 def get_icon(icon_name):
     """Retrieve the path of an icon inside the package."""
     return Gtk.Image.new_from_file(get_icon_path(icon_name))
-
-
-def get_machine_template_path():
-    """Retrieve the path of an icon inside the package."""
-    with importlib.resources.path(machines) as path:
-        return path
