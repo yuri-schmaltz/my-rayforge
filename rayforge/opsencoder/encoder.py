72c09e14cd2c30cfeeaa798d4c7a181df7013ae7
Yuri Schmaltz <security@yuri-schmaltz.dev>
2025-02-18 10:14:42 +0100

    update OpsEncoder docstring

--- rayforge/opsencoder/encoder.py
+++ rayforge/opsencoder/encoder.py
@@ -5,14 +5,12 @@ from ..models.machine import Machine
 
 class OpsEncoder(ABC):
     """
-    Encodes Ops while respecting embedded state commands.
-    Encoding Ops means: Converting it to an output format.
-    Output cut be an image (a cairo surface), a G-code string,
-    or any other machine format.
-    Maintains context during encoding (current color, power, etc.).
+    Transforms an Ops object into something else.
+    Examples:
+
+    - Ops to image (a cairo surface)
+    - Ops to a G-code string
     """
     @abstractmethod
-    def encode(self,
-               pos: Ops,
-               machine: Machine) -> object:
+    def encode(self, pos: Ops, machine: Machine) -> object:
         pass
