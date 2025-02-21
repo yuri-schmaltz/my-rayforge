c65b7db8fa426bdfec0b7243fdd4a961c6de732a
Yuri Schmaltz <security@yuri-schmaltz.dev>
2025-02-21 12:14:49 +0100

    cairoencoder now uses Ops.segments()

--- rayforge/opsencoder/cairoencoder.py
+++ rayforge/opsencoder/cairoencoder.py
@@ -1,8 +1,12 @@
+import os
 import math
 import cairo
 from ..models.ops import Ops, MoveToCommand, LineToCommand, ArcToCommand
 from ..models.machine import Machine
 from .encoder import OpsEncoder
+
+
+SHOW_TRAVEL_MOVES = bool(os.environ.get('SHOW_TRAVEL_MOVES', False))
 
 
 class CairoEncoder(OpsEncoder):
@@ -30,71 +34,71 @@ class CairoEncoder(OpsEncoder):
 
         # Apply coordinate scaling and line width
         ctx.scale(scale_x, scale_y)
-        ctx.set_line_width(1 / scale_x)  # 1-pixel line width post-scaling
+        ctx.set_hairline(True)
 
-        # Track rendering state
-        active_path = False
         prev_point = 0, ymax
+        for segment in ops.segments():
+            print(segment)
+            for cmd in segment:
+                match cmd, cmd.end:
+                    case MoveToCommand(), (x, y):
+                        adjusted_y = ymax - y
 
-        for cmd in ops:
-            match cmd, cmd.end:
-                case MoveToCommand(), (x, y):
-                    if active_path:
-                        ctx.set_source_rgb(1, 0, 1)
-                        ctx.stroke()  # Finalize previous path
+                        # Paint the travel move. We do not have to worry that
+                        # there may be any unpainted path before it, because
+                        # Ops.segments() ensures that each travel move opens
+                        # a new segment.
+                        if SHOW_TRAVEL_MOVES:
+                            ctx.move_to(*prev_point)
+                            ctx.set_source_rgb(.8, .8, .8)
+                            ctx.line_to(x, adjusted_y)
+                            ctx.stroke()
+                        else:
+                            ctx.move_to(x, adjusted_y)
 
-                    adjusted_y = ymax - y
-                    if False:  # debug toggle for painting travel moves
-                        ctx.move_to(*prev_point)
-                        ctx.set_source_rgb(.8, .8, .8)
+                        prev_point = x, adjusted_y
+
+                    case LineToCommand(), (x, y):
+                        adjusted_y = ymax-y
                         ctx.line_to(x, adjusted_y)
+                        prev_point = x, adjusted_y
+
+                    case ArcToCommand(), (x, y):
+                        # x, y: absolute values
+                        # i, j: relative position of arc center from start point.
+                        adjusted_y = ymax-y
+
+                        # Start point is the x, y of the previous operation.
+                        start_x, start_y = prev_point
+
+                        # Draw the arc in the correct direction
+                        i, j = cmd.center_offset
+                        center_x = start_x+i
+                        center_y = start_y+j
+                        radius = math.dist(prev_point, (center_x, center_y))
+                        angle1 = math.atan2(start_y - center_y, start_x - center_x)
+                        angle2 = math.atan2(adjusted_y - center_y, x - center_x)
                         ctx.stroke()
+                        ctx.set_source_rgb(0, 0, 1)
+                        if cmd.clockwise:
+                            ctx.arc(center_x, center_y, radius, angle1, angle2)
+                        else:
+                            ctx.arc_negative(
+                                center_x,
+                                center_y,
+                                radius,
+                                angle1,
+                                angle2
+                            )
+                        ctx.stroke()
+                        ctx.move_to(x, adjusted_y)
+                        ctx.set_source_rgb(1, 0, 1)
 
-                    prev_point = x, adjusted_y
-                    active_path = True
+                        prev_point = x, adjusted_y
 
-                case LineToCommand(), (x, y):
-                    adjusted_y = ymax-y
-                    ctx.move_to(*prev_point)
-                    ctx.set_source_rgb(1, 0, 1)
-                    ctx.line_to(x, adjusted_y)
-                    prev_point = x, adjusted_y
-                    active_path = True
+                    case _:
+                        pass  # ignore unsupported operations
 
-                case ArcToCommand(), (x, y):
-                    # x, y: absolute values
-                    # i, j: relative position of arc center from start point.
-                    adjusted_y = ymax-y
-                    ctx.move_to(*prev_point)
-                    ctx.set_source_rgb(1, 0, 0)
-
-                    # Start point is the x, y of the previous operation.
-                    start_x, start_y = prev_point
-
-                    # Draw the arc in the correct direction
-                    i, j = cmd.center_offset
-                    center_x = start_x+i
-                    center_y = start_y+j
-                    radius = math.dist(prev_point, (center_x, center_y))
-                    angle1 = math.atan2(start_y - center_y, start_x - center_x)
-                    angle2 = math.atan2(adjusted_y - center_y, x - center_x)
-                    if cmd.clockwise:
-                        ctx.arc(center_x, center_y, radius, angle1, angle2)
-                    else:
-                        ctx.arc_negative(
-                            center_x,
-                            center_y,
-                            radius,
-                            angle1,
-                            angle2
-                        )
-
-                    prev_point = x, adjusted_y
-                    active_path = True
-
-                case _:
-                    pass  # ignore unsupported operations
-
-        # Stroke any remaining open path
-        if active_path:
+            # Draw the segment.
+            ctx.set_source_rgb(1, 0, 1)
             ctx.stroke()
