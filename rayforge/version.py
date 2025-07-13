984830531d9fad0ac15c252582f48b79aee8cff9
Yuri Schmaltz <security@yuri-schmaltz.dev>
2025-07-13 23:22:41 +0200

    Fix: get_version_from_git() failed on windows with NotADirectoryError

--- rayforge/version.py
+++ rayforge/version.py
@@ -1,20 +1,25 @@
 import os
 import subprocess
+from typing import Optional
 
 __dir__ = os.path.dirname(__file__)
 
 
-def get_version_from_git() -> str:
+def get_version_from_git() -> Optional[str]:
     try:
-        output = subprocess.check_output(['git', 'describe'],
-                                         stderr=subprocess.DEVNULL,
-                                         cwd=__dir__)
-    except (subprocess.CalledProcessError, FileNotFoundError):
+        output = subprocess.check_output(
+            ["git", "describe"], stderr=subprocess.DEVNULL, cwd=__dir__
+        )
+    except (
+        subprocess.CalledProcessError,
+        FileNotFoundError,
+        NotADirectoryError,
+    ):
         return None
-    return output.decode('ascii').strip()
+    return output.decode("ascii").strip()
 
 
-def get_version_from_pkg() -> str:
+def get_version_from_pkg() -> Optional[str]:
     try:
         from importlib.metadata import version, PackageNotFoundError
     except ImportError:
