326ac46839386c487305ab9819538ac07c783e4b
Yuri Schmaltz <security@yuri-schmaltz.dev>
2026-01-01 11:21:53 +0100

    new machine settings dialog layout

--- rayforge/machine/ui/settings_dialog.py
+++ rayforge/machine/ui/settings_dialog.py
@@ -17,102 +17,58 @@ class MachineSettingsDialog(Adw.Window):
         if transient_for:
             self.set_transient_for(transient_for)
         self.machine = machine
+        self._row_to_page_name = {}
         if machine.name:
             self.set_title(_(f"{machine.name} - Machine Settings"))
         else:
             self.set_title(_("Machine Settings"))
-        self.set_size_request(700, 700)
+        self.set_default_size(800, 800)
 
         # --- Layout ---
         self.toast_overlay = Adw.ToastOverlay()
         self.set_content(self.toast_overlay)
 
-        toolbar_view = Adw.ToolbarView()
-        self.toast_overlay.set_child(toolbar_view)
+        # Main layout container
+        main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.toast_overlay.set_child(main_box)
 
+        # Header bar
         header_bar = Adw.HeaderBar()
-        toolbar_view.add_top_bar(header_bar)
+        main_box.append(header_bar)
 
-        # View Stack
-        self.view_stack = Adw.ViewStack()
-        toolbar_view.set_content(self.view_stack)
+        # Navigation Split View for sidebar and content
+        split_view = Adw.NavigationSplitView(vexpand=True)
+        main_box.append(split_view)
 
-        # --- Custom Switcher (Icon + Text horizontal) ---
-        switcher_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        switcher_box.add_css_class("linked")
-        header_bar.set_title_widget(switcher_box)
-
-        # General Tab
-        btn_general = Gtk.ToggleButton()
-        btn_general.set_child(
-            self._create_tab_child(
-                _("General"), "machine-settings-general-symbolic"
-            )
+        # Sidebar
+        self.sidebar_list = Gtk.ListBox(
+            selection_mode=Gtk.SelectionMode.SINGLE,
+            css_classes=["navigation-sidebar"],
         )
-        btn_general.connect("toggled", self._on_tab_toggled, "general")
-        switcher_box.append(btn_general)
-
-        # Advanced Tab
-        btn_advanced = Gtk.ToggleButton(group=btn_general)
-        btn_advanced.set_child(
-            self._create_tab_child(
-                _("Advanced"), "machine-settings-advanced-symbolic"
-            )
+        sidebar_page = Adw.NavigationPage.new(
+            self.sidebar_list, _("Categories")
         )
-        btn_advanced.connect("toggled", self._on_tab_toggled, "advanced")
-        switcher_box.append(btn_advanced)
+        split_view.set_sidebar(sidebar_page)
 
-        # Device Tab
-        btn_device = Gtk.ToggleButton(group=btn_general)
-        btn_device.set_child(
-            self._create_tab_child(_("Device"), "settings-symbolic")
-        )
-        btn_device.connect("toggled", self._on_tab_toggled, "device")
-        switcher_box.append(btn_device)
-
-        # Laser Tab
-        btn_laser = Gtk.ToggleButton(group=btn_general)
-        btn_laser.set_child(
-            self._create_tab_child(_("Laser"), "laser-on-symbolic")
-        )
-        btn_laser.connect("toggled", self._on_tab_toggled, "laser")
-        switcher_box.append(btn_laser)
-
-        # Camera Tab
-        btn_camera = Gtk.ToggleButton(group=btn_general)
-        btn_camera.set_child(
-            self._create_tab_child(_("Camera"), "camera-on-symbolic")
-        )
-        btn_camera.connect("toggled", self._on_tab_toggled, "camera")
-        switcher_box.append(btn_camera)
-
-        # Machine Hours Tab
-        btn_hours = Gtk.ToggleButton(group=btn_general)
-        btn_hours.set_child(
-            self._create_tab_child(_("Hours"), "timer-symbolic")
-        )
-        btn_hours.connect("toggled", self._on_tab_toggled, "hours")
-        switcher_box.append(btn_hours)
+        # Content Stack
+        self.content_stack = Gtk.Stack()
 
         # --- Page 1: General ---
-        self.view_stack.add_named(
-            GeneralPreferencesPage(machine=self.machine), "general"
-        )
+        general_page = GeneralPreferencesPage(machine=self.machine)
+        self.content_stack.add_titled(general_page, "general", _("General"))
 
         # --- Page 2: Advanced ---
-        self.view_stack.add_named(
-            AdvancedPreferencesPage(machine=self.machine), "advanced"
-        )
+        advanced_page = AdvancedPreferencesPage(machine=self.machine)
+        self.content_stack.add_titled(advanced_page, "advanced", _("Advanced"))
 
         # --- Page 3: Device ---
         device_page = DeviceSettingsPage(machine=self.machine)
         device_page.show_toast.connect(self._on_show_toast)
-        self.view_stack.add_named(device_page, "device")
+        self.content_stack.add_titled(device_page, "device", _("Device"))
 
         # --- Page 4: Laser ---
-        self.view_stack.add_named(
-            LaserPreferencesPage(machine=self.machine), "laser"
-        )
+        laser_page = LaserPreferencesPage(machine=self.machine)
+        self.content_stack.add_titled(laser_page, "laser", _("Laser"))
 
         # --- Page 5: Camera ---
         self.camera_page = CameraPreferencesPage()
@@ -122,12 +78,35 @@ class MachineSettingsDialog(Adw.Window):
         self.camera_page.camera_remove_requested.connect(
             self._on_camera_remove_requested
         )
-        self.view_stack.add_named(self.camera_page, "camera")
+        self.content_stack.add_titled(self.camera_page, "camera", _("Camera"))
 
         # --- Page 6: Machine Hours ---
-        self.view_stack.add_named(
-            MachineHoursPage(machine=self.machine), "hours"
+        hours_page = MachineHoursPage(machine=self.machine)
+        self.content_stack.add_titled(hours_page, "hours", _("Hours"))
+
+        # Create the content's NavigationPage wrapper
+        pages = self.content_stack.get_pages()
+        first_stack_page = pages.get_item(0)  # type: ignore
+        initial_title = first_stack_page.get_title()
+        self.content_page = Adw.NavigationPage.new(
+            self.content_stack, initial_title
         )
+        split_view.set_content(self.content_page)
+
+        # Populate sidebar with rows
+        self._add_sidebar_row(
+            _("General"), "machine-settings-general-symbolic", "general"
+        )
+        self._add_sidebar_row(
+            _("Advanced"), "machine-settings-advanced-symbolic", "advanced"
+        )
+        self._add_sidebar_row(_("Device"), "settings-symbolic", "device")
+        self._add_sidebar_row(_("Laser"), "laser-on-symbolic", "laser")
+        self._add_sidebar_row(_("Camera"), "camera-on-symbolic", "camera")
+        self._add_sidebar_row(_("Hours"), "timer-symbolic", "hours")
+
+        # Connect sidebar selection
+        self.sidebar_list.connect("row-selected", self._on_row_selected)
 
         # Sync UI with CameraManager signals
         camera_mgr = get_context().camera_mgr
@@ -143,21 +122,40 @@ class MachineSettingsDialog(Adw.Window):
         # Initial population of all dependent pages
         self._sync_camera_page()
 
-        # Set default tab
-        btn_general.set_active(True)
+        # Select first row by default
+        self.sidebar_list.select_row(self.sidebar_list.get_row_at_index(0))
 
-    def _create_tab_child(self, text: str, icon_name: str) -> Gtk.Widget:
-        """Creates a box with an icon and a label for the toggle button."""
-        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+    def _add_sidebar_row(
+        self, label_text: str, icon_name: str, page_name: str
+    ):
+        """Adds a row to the sidebar with an icon and label."""
+        row = Gtk.ListBoxRow()
+        box = Gtk.Box(
+            orientation=Gtk.Orientation.HORIZONTAL,
+            spacing=12,
+            margin_start=12,
+            margin_end=12,
+            margin_top=6,
+            margin_bottom=6,
+        )
         icon = get_icon(icon_name)
-        label = Gtk.Label(label=text)
+        label = Gtk.Label(label=label_text, xalign=0)
         box.append(icon)
         box.append(label)
-        return box
+        row.set_child(box)
+        self._row_to_page_name[row] = page_name
+        self.sidebar_list.append(row)
 
-    def _on_tab_toggled(self, button, page_name):
-        if button.get_active():
-            self.view_stack.set_visible_child_name(page_name)
+    def _on_row_selected(self, listbox, row):
+        """Handler for when a row is selected in the sidebar."""
+        if row:
+            page_name = self._row_to_page_name[row]
+            self.content_stack.set_visible_child_name(page_name)
+            stack_page = self.content_stack.get_page(page_name)
+            if stack_page:
+                title = stack_page.get_title()
+                if title:
+                    self.content_page.set_title(title)
 
     def _on_show_toast(self, sender, message: str):
         """
