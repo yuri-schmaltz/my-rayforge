6919d57b9c1c85701aca4b0d9622a06181b82177
Yuri Schmaltz <security@yuri-schmaltz.dev>
2026-01-30 11:42:29 +0100

    fix: conflicting shortcuts for 'save as...' and simulation mode. Change simulation mode shortcut to F11

--- rayforge/ui_gtk/actions.py
+++ rayforge/ui_gtk/actions.py
@@ -375,7 +375,7 @@ class ActionManager:
             "win.toggle_gcode_preview": "<Primary><Shift>g",
             "win.toggle_travel_view": "<Primary><Shift>t",
             "win.show_3d_view": "F12",
-            "win.simulate_mode": "<Primary><Shift>s",
+            "win.simulate_mode": "F11",
             "win.view_top": "1",
             "win.view_front": "2",
             "win.view_iso": "7",
