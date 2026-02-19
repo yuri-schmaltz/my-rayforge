eb330bf35947e5c87beebe2b5be806fd49f2c481
Yuri Schmaltz <security@yuri-schmaltz.dev>
2026-02-19 14:24:10 +0100

    fix path to windows icon

--- scripts/win/win_create_icon.sh
+++ scripts/win/win_create_icon.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 set -e
 
-SVG_PATH="website/content/assets/favicon.png"
+SOURCE_PATH="website/static/assets/favicon.png"
 ICO_PATH="rayforge.ico"
 
-if [ ! -f "$SVG_PATH" ]; then
-    echo "FATAL: Icon source file not found at $SVG_PATH"
+if [ ! -f "$SOURCE_PATH" ]; then
+    echo "FATAL: Icon source file not found at $SOURCE_PATH"
     exit 1
 fi
 
-echo "--- Generating Windows Icon ($ICO_PATH) from SVG ---"
+echo "--- Generating Windows Icon ($ICO_PATH) ---"
 
-# Use ImageMagick to render the SVG to multiple PNG sizes
-# and then combine them into a single .ico file.
-# Using a high density ensures the initial render from SVG is high quality.
-magick -density 300 -background transparent "$SVG_PATH" \
+magick -density 300 -background transparent "$SOURCE_PATH" \
        -define icon:auto-resize=256,64,48,32,16 \
        "$ICO_PATH"
 
