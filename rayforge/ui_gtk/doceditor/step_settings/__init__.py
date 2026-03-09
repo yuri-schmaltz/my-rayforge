ab51a0fc948a77821006869730ef31669f462460
Yuri Schmaltz <security@yuri-schmaltz.dev>
2026-03-10 00:18:52 +0100

    forgot to remove old step files in a previous commit

--- rayforge/ui_gtk/doceditor/step_settings/__init__.py
+++ rayforge/ui_gtk/doceditor/step_settings/__init__.py
@@ -1,17 +1,7 @@
 from .base import StepComponentSettingsWidget
-from .crop import CropTransformerSettingsWidget
-from .multipass import MultiPassSettingsWidget
-from .optimize import OptimizeSettingsWidget
-from .overscan import OverscanSettingsWidget
 from .placeholder import PlaceholderSettingsWidget
-from .smooth import SmoothSettingsWidget
 
 __all__ = [
-    "CropTransformerSettingsWidget",
-    "MultiPassSettingsWidget",
-    "OptimizeSettingsWidget",
-    "OverscanSettingsWidget",
     "PlaceholderSettingsWidget",
-    "SmoothSettingsWidget",
     "StepComponentSettingsWidget",
 ]
