7bd447a5380abf658ae5342ac2748b8dbe9b0df4
Yuri Schmaltz <security@yuri-schmaltz.dev>
2026-03-16 22:56:49 +0100

    camera selection dialog now has clearer indication to show that scrolling to other cameras works

--- rayforge/ui_gtk/camera/selection_dialog.py
+++ rayforge/ui_gtk/camera/selection_dialog.py
@@ -1,3 +1,4 @@
+import logging
 from gi.repository import Gtk, Adw, GdkPixbuf
 from typing import Optional, Literal
 from gettext import gettext as _
@@ -5,6 +6,8 @@ from ...camera.models.camera import Camera
 from ...camera.controller import CameraController
 from ...context import get_context
 from ..shared.gtk import apply_css
+
+logger = logging.getLogger(__name__)
 
 
 class CameraSelectionDialog(Adw.MessageDialog):
@@ -28,12 +31,15 @@ class CameraSelectionDialog(Adw.MessageDialog):
             close_response="cancel",
             **kwargs,
         )
-        self.set_size_request(400, 300)
+        self.set_size_request(450, 350)
         self.selected_device_id: Optional[str] = None
 
         apply_css("""
             .rounded-image {
                 border-radius: 8px;
+            }
+            .nav-button {
+                padding: 12px;
             }
         """)
 
@@ -43,12 +49,39 @@ class CameraSelectionDialog(Adw.MessageDialog):
         self.carousel.set_allow_scroll_wheel(True)
         self.carousel.set_allow_long_swipes(True)
         self.carousel.set_interactive(True)
-        self.carousel.set_margin_start(12)
-        self.carousel.set_margin_end(12)
-        self.carousel.set_margin_top(12)
-        self.carousel.set_margin_bottom(12)
 
-        self.set_extra_child(self.carousel)
+        self.prev_button = Gtk.Button(icon_name="go-previous-symbolic")
+        self.prev_button.add_css_class("nav-button")
+        self.prev_button.add_css_class("flat")
+        self.prev_button.set_sensitive(False)
+        self.prev_button.set_valign(Gtk.Align.CENTER)
+        self.prev_button.connect("clicked", self.on_prev_clicked)
+
+        self.next_button = Gtk.Button(icon_name="go-next-symbolic")
+        self.next_button.add_css_class("nav-button")
+        self.next_button.add_css_class("flat")
+        self.next_button.set_sensitive(False)
+        self.next_button.set_valign(Gtk.Align.CENTER)
+        self.next_button.connect("clicked", self.on_next_clicked)
+
+        carousel_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        carousel_box.append(self.prev_button)
+        carousel_box.append(self.carousel)
+        carousel_box.append(self.next_button)
+
+        self.indicator = Adw.CarouselIndicatorDots()
+        self.indicator.set_carousel(self.carousel)
+        self.indicator.set_margin_bottom(6)
+
+        content_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        content_box.append(carousel_box)
+        content_box.append(self.indicator)
+        content_box.set_margin_start(12)
+        content_box.set_margin_end(12)
+        content_box.set_margin_top(12)
+        content_box.set_margin_bottom(6)
+
+        self.set_extra_child(content_box)
 
         self.add_response("cancel", _("Cancel"))
         self.set_response_enabled("cancel", True)
@@ -62,6 +95,10 @@ class CameraSelectionDialog(Adw.MessageDialog):
             self.list_configured_cameras()
 
         self.carousel.connect("page-changed", self.on_page_changed)
+
+        key_controller = Gtk.EventControllerKey()
+        key_controller.connect("key-pressed", self.on_key_pressed)
+        self.add_controller(key_controller)
 
     def list_configured_cameras(self):
         camera_mgr = get_context().camera_mgr
@@ -82,6 +119,7 @@ class CameraSelectionDialog(Adw.MessageDialog):
             first_child = self.carousel.get_nth_page(0)
             self.carousel.scroll_to(first_child, True)
             self.selected_device_id = self.available_devices[0]
+            self._update_nav_buttons()
 
     def _add_camera_page(
         self, controller: CameraController, device_id: str, name: str
@@ -168,12 +206,14 @@ class CameraSelectionDialog(Adw.MessageDialog):
             first_child = self.carousel.get_nth_page(0)
             self.carousel.scroll_to(first_child, True)
             self.selected_device_id = self.available_devices[0]
+            self._update_nav_buttons()
 
     def on_page_changed(self, carousel, page_number):
         if 0 <= page_number < len(self.available_devices):
             self.selected_device_id = self.available_devices[page_number]
         else:
             self.selected_device_id = None
+        self._update_nav_buttons()
 
     def on_carousel_item_clicked(self, gesture, n_press, x, y, device_id):
         self.selected_device_id = device_id
@@ -185,5 +225,32 @@ class CameraSelectionDialog(Adw.MessageDialog):
         box.add_css_class("card")
 
     def on_carousel_item_hover_leave(self, motion_controller, box):
-        # Remove the "card" style class
         box.remove_css_class("card")
+
+    def on_prev_clicked(self, button):
+        current = self.carousel.get_position()
+        if current > 0:
+            page = self.carousel.get_nth_page(int(current) - 1)
+            self.carousel.scroll_to(page, True)
+
+    def on_next_clicked(self, button):
+        n_pages = self.carousel.get_n_pages()
+        current = self.carousel.get_position()
+        if current < n_pages - 1:
+            page = self.carousel.get_nth_page(int(current) + 1)
+            self.carousel.scroll_to(page, True)
+
+    def on_key_pressed(self, controller, keyval, keycode, state):
+        if keyval == 65361:
+            self.on_prev_clicked(None)
+            return True
+        elif keyval == 65363:
+            self.on_next_clicked(None)
+            return True
+        return False
+
+    def _update_nav_buttons(self):
+        n_pages = self.carousel.get_n_pages()
+        current = int(self.carousel.get_position())
+        self.prev_button.set_sensitive(current > 0)
+        self.next_button.set_sensitive(current < n_pages - 1)
