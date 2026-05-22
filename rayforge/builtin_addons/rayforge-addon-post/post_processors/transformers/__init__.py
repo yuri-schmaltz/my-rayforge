02123726b4b82b88bf135a2e642a67c1ac8dd1ea
Yuri Schmaltz <security@yuri-schmaltz.dev>
2026-05-22 11:15:45 +0200

    Migrate the Optimize transformer to use raygeo.ops.optimize_travel()

--- rayforge/builtin_addons/rayforge-addon-post/post_processors/transformers/__init__.py
+++ rayforge/builtin_addons/rayforge-addon-post/post_processors/transformers/__init__.py
@@ -9,12 +9,7 @@ from .crop_transformer import CropTransformer
 from .lead_in_out_transformer import LeadInOutTransformer
 from .merge_lines_transformer import MergeLinesTransformer
 from .multipass_transformer import MultiPassTransformer
-from .optimize_transformer import (
-    Optimize,
-    greedy_order_segments,
-    kdtree_order_segments,
-    two_opt,
-)
+from .optimize_transformer import Optimize
 from .overscan_transformer import OverscanTransformer
 from .smooth_transformer import Smooth
 from .tabs_transformer import TabOpsTransformer
@@ -28,7 +23,4 @@ __all__ = [
     "OverscanTransformer",
     "Smooth",
     "TabOpsTransformer",
-    "greedy_order_segments",
-    "two_opt",
-    "kdtree_order_segments",
 ]
